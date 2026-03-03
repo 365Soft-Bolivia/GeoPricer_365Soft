2d13e0058e2895d6379f807291be52cb3b663df4
Jhurguen777 <jhurguen94@gmail.com>
2026-03-03 10:47:32 -0400

    fixeo rapido5

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 import path from 'path';
-import wayfinder from '@laravel/vite-plugin-wayfinder';
+import { fileURLToPath } from 'url';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     resolve: {
